01a2f3996ebb151bf9abfdb855881477b22f77af
Larry Olguin <iraqwarvet31@gmail.com>
2021-08-07 10:08:18 -0700

    moved mongo connection to node server

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,17 +1,3 @@
-const mongoose = require('mongoose');
-
-const mongoURI = process.env.MONGO_URI
-
-const connectMongoDB = mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-connectMongoDB
-    .then(() => console.log(`Connected to: ${mongoURI}`))
-    .catch((err) => {
-        console.log(`There was a problem connecting to ${mongoURI}`);
-        console.log(err);
-    })
-
-module.exports = connectMongoDB;
+module.exports = {
+    url: process.env.MONGO_URI
+}
--- server/index.js
+++ server/index.js
@@ -2,14 +2,30 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
 const connectMongoDB = require('./config/db.config');
+const db = require('./config/db.config');
 
 const app = express();
 
 const port = 3000;
 const DIR_NAME = path.resolve(__dirname, '..', 'public');
 
+// Serve static files
 app.use(express.static(DIR_NAME));
+
+// parse request contents
+app.use(express.json());
+
+// Connect to mongodb database
+mongoose
+  .connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(`Connected to: ${db.url}`))
+  .catch((err) => {
+      console.log(`There was a problem connecting to ${db.url}`);
+      console.log(err);
+  })
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
