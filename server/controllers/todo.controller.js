d2f9b65ee5570e986a3f10a639b41fe89ca6ca41
Larry Olguin <iraqwarvet31@gmail.com>
2021-08-07 11:28:39 -0700

    add createTodo function

--- server/controllers/todo.controller.js
+++ server/controllers/todo.controller.js
@@ -5,3 +5,14 @@ exports.readAllTodos = (req, res) => {
         .then((docs) => res.status(200).send(docs))
         .catch((err) => res.sendStatus(500));
 }
+
+exports.createTodo = (req, res) => {
+    const todo = new Todo({
+        task: req.body.task,
+        completed: req.body.completed
+    });
+    console.log('todo')
+    // todo.save(todo)
+    //     .then(() => res.status(200).send(data))
+    //     .catch((err) => res.status(500).send(err))
+}
