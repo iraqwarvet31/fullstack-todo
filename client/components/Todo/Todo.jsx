de2d1cca3b5c584d9effaf42dde7cfd9cd5a1065
Larry Olguin <iraqwarvet31@gmail.com>
2021-08-15 12:13:00 -0700

    fix toggle bug

--- client/components/Todo/Todo.jsx
+++ client/components/Todo/Todo.jsx
@@ -1,35 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { AiTwotoneEdit as EditBtn } from 'react-icons/ai';
-import { RiDeleteBin5Line as DeleteBtn } from 'react-icons/ri';
+import { AiTwotoneEdit as EditBtn } from "react-icons/ai";
+import { RiDeleteBin5Line as DeleteBtn } from "react-icons/ri";
 
-import './Todo.css';
+import "./Todo.css";
 
-const Todo = ({ task, item, deleteTodo, id, completedTasks, setCompleteTasks }) => {
+const Todo = ({
+  task,
+  item,
+  deleteTodo,
+  id,
+  completedTasks,
+  setCompleteTasks,
+}) => {
   const [isComplete, setComplete] = useState(false);
 
   const toggleTasks = () => {
     if (!isComplete) {
-      setCompleteTasks([...completedTasks, item]);
+      setCompleteTasks([
+        ...completedTasks,
+        { _id: id, task: item.task, completed: !isComplete },
+      ]);
     } else {
-      setCompleteTasks(completedTasks.filter(ele => ele._id !== id))
+      setCompleteTasks(completedTasks.filter((ele) => ele._id !== id));
     }
-    setComplete(val => !val)
-  }
-
+    setComplete((val) => !val);
+  };
+  console.log(completedTasks);
   return (
-    <li 
-      className={isComplete ? 'complete list-item' : 'list-item'} 
+    <li
+      className={isComplete ? "complete list-item" : "list-item"}
       onClick={toggleTasks}
     >
       <span>{task}</span>
-      <DeleteBtn
-        className="delete-icon"
-        onClick={() => deleteTodo(id)}
-      />
+      <DeleteBtn className="delete-icon" onClick={() => deleteTodo(id)} />
       <EditBtn />
     </li>
-  )
+  );
 };
 
 export default Todo;
